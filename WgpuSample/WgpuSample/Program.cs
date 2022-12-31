18e6b74fb7995a32ff625c1774d611ff682b11e9
ikorin24 <sunshinexxab@yahoo.co.jp>
2022-12-31 10:30:27 +0900

    update

--- WgpuSample/WgpuSample/Program.cs
+++ WgpuSample/WgpuSample/Program.cs
@@ -29,11 +29,11 @@ internal class Program
         };
         _renderPipeline = EngineCore.elffy_add_render_pipeline(screen, in pipelineInfo);
 
-        var vertices = stackalloc PosColorVertex[3]
+        ReadOnlySpan<PosColorVertex> vertices = stackalloc PosColorVertex[4]
         {
             new()
             {
-                Position = new(0.0f, 0.5f, 0.0f),
+                Position = new(-0.5f, 0.5f, 0.0f),
                 Color = new(1.0f, 0.0f, 0.0f),
             },
             new()
@@ -46,29 +46,59 @@ internal class Program
                 Position = new(0.5f, -0.5f, 0.0f),
                 Color = new(0.0f, 0.0f, 1.0f),
             },
+            new()
+            {
+                Position = new(0.5f, 0.5f, 0.0f),
+                Color = new(0.0f, 0.0f, 1.0f),
+            },
         };
-        var contents = new Sliceffi<byte>((byte*)vertices, (nuint)(3 * sizeof(PosColorVertex)));
-        _buffer = EngineCore.elffy_create_buffer_init(screen, contents, BufferUsages.VERTEX);
+        ReadOnlySpan<uint> indices = stackalloc uint[6] { 0, 1, 2, 2, 3, 0 };
+        fixed(uint* i = indices)
+        fixed(PosColorVertex* v = vertices) {
+            var vContents = new Sliceffi<byte>((byte*)v, (nuint)(vertices.Length * sizeof(PosColorVertex)));
+            _vertexBuffer = EngineCore.elffy_create_buffer_init(screen, vContents, BufferUsages.VERTEX);
+            _vertexCount = (uint)vertices.Length;
+
+            var iContents = new Sliceffi<byte>((byte*)i, (nuint)(indices.Length * sizeof(uint)));
+            _indexBuffer = EngineCore.elffy_create_buffer_init(screen, iContents, BufferUsages.INDEX);
+            _indexCount = (uint)indices.Length;
+        }
     }
 
     private static RenderPipelineHandle _renderPipeline;
-    private static BufferHandle _buffer;
+    private static BufferHandle _vertexBuffer;
+    private static uint _vertexCount;
+    private static BufferHandle _indexBuffer;
+    private static uint _indexCount;
 
     private static void OnRender(HostScreenHandle screen, RenderPassHandle renderPass)
     {
         EngineCore.elffy_set_pipeline(renderPass, _renderPipeline);
-        unsafe {
-            EngineCore.elffy_draw_buffer(
-                render_pass: renderPass,
-                vertex_buffer: new()
-                {
-                    buffer_slice = new() { buffer = _buffer, range = RangeBoundsU64ffi.All },
-                    slot = 0,
-                },
-                vertices_range: new RangeU32ffi(0, 3),
-                instances_range: new(0, 1)
-                );
-        }
+        //EngineCore.elffy_draw_buffer(
+        //    render_pass: renderPass,
+        //    vertex_buffer: new()
+        //    {
+        //        buffer_slice = new() { buffer = _vertexBuffer, range = RangeBoundsU64ffi.All },
+        //        slot = 0,
+        //    },
+        //    vertices_range: new RangeU32ffi(0, _vertexCount),
+        //    instances_range: new(0, 1)
+        //);
+        EngineCore.elffy_draw_buffer_indexed(
+            render_pass: renderPass,
+            vertex_buffer: new()
+            {
+                buffer_slice = new() { buffer = _vertexBuffer, range = RangeBoundsU64ffi.All },
+                slot = 0,
+            },
+            index_buffer: new()
+            {
+                buffer_slice = new() { buffer = _indexBuffer, range = RangeBoundsU64ffi.All },
+                format = IndexFormat.Uint32,
+            },
+            indices_range: new RangeU32ffi(0, _indexCount),
+            instances_range: new(0, 1)
+        );
     }
 
     private unsafe static Sliceffi<byte> ShaderSource
@@ -96,7 +126,7 @@ fn vs_main(vin: VertexInput) -> VertexOutput {
 
 @fragment
 fn fs_main(fin: VertexOutput) -> @location(0) vec4<f32> {
-    return vec4<f32>(fin.color.r, 0.0, 1.0, 1.0);
+    return vec4<f32>(fin.color, 1.0);
 }
 
 """u8;
