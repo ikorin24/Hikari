22af450c95e58a5fcf80de54040a96d3793cce8f
ikorin24 <sunshinexxab@yahoo.co.jp>
2023-01-09 21:53:20 +0900

    update

--- WgpuSample/WgpuSample/EngineCore.Native.cs
+++ WgpuSample/WgpuSample/EngineCore.Native.cs
@@ -1,8 +1,5 @@
 ﻿#nullable enable
 using u8 = System.Byte;
-using u16 = System.UInt16;
-using u32 = System.UInt32;
-using u64 = System.UInt64;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -12,7 +9,8 @@ namespace WgpuSample;
 
 static unsafe partial class EngineCore
 {
-    private const string CoreDll = "elffycore";
+    private const string DllDir = "platform/x86_64-windows/";
+    private const string CoreDll = $"{DllDir}elffycore";
 
     [LibraryImport(CoreDll), UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
     internal static partial void elffy_engine_start(
@@ -139,49 +137,4 @@ static unsafe partial class EngineCore
     internal static partial void elffy_draw_buffers_indexed(
         RenderPassHandle render_pass,
         DrawBuffersIndexedArg* arg);
-
-
-
-
-
-
-    //[LibraryImport(EngineCoreDll), UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    //public static partial RenderPipelineHandle elffy_add_render_pipeline(
-    //	HostScreenHandle screen,
-    //	in RenderPipelineInfo render_pipeline);
-
-    //[LibraryImport(EngineCoreDll), UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    //public static partial BufferHandle elffy_create_buffer_init(
-    //	HostScreenHandle screen,
-    //	Sliceffi<u8> contents,
-    //	BufferUsages usage);
-
-    //[LibraryImport(EngineCoreDll), UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    //public static partial void elffy_set_pipeline(
-    //	RenderPassHandle render_pass,
-    //	RenderPipelineHandle render_pipeline);
-
-    //[LibraryImport(EngineCoreDll), UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    //public static partial void elffy_draw_buffer(
-    //	RenderPassHandle render_pass,
-    //	in SlotBufferSliceffi vertex_buffer,
-    //	in RangeU32ffi vertices_range,
-    //	in RangeU32ffi instances_range);
-
-    //[LibraryImport(EngineCoreDll), UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    //public static partial void elffy_draw_buffer_indexed(
-    //	RenderPassHandle render_pass,
-    //	in SlotBufferSliceffi vertex_buffer,
-    //	in IndexBufferSliceffi index_buffer,
-    //	in RangeU32ffi indices_range,
-    //	in RangeU32ffi instances_range);
-
-    //[LibraryImport(EngineCoreDll), UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    //public static partial void elffy_draw_buffers_indexed(
-    //	RenderPassHandle render_pass,
-    //	Sliceffi<SlotBufferSliceffi> vertex_buffers,
-    //	in IndexBufferSliceffi index_buffer,
-    //	in RangeU32ffi indices_range,
-    //	in RangeU32ffi instances_range);
-
 }
