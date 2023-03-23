3de70b1bcc6b2e254f9aa76cdd812e3deb74c90c
ikorin24 <sunshinexxab@yahoo.co.jp>
2023-03-24 01:40:33 +0900

    update

---
+++ WgpuSample/Elffy.Core/FrameObject.cs
@@ -0,0 +1,76 @@
+﻿#nullable enable
+using System;
+using System.Diagnostics;
+
+namespace Elffy;
+
+public abstract class FrameObject
+{
+    private readonly HostScreen _screen;
+    private string? _name;
+    private readonly SubscriptionBag _subscriptions = new SubscriptionBag();
+
+    public HostScreen Screen => _screen;
+
+    public string? Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public SubscriptionRegister Subscriptions => _subscriptions.Register;
+
+    public abstract bool IsFrozen { get; set; }
+    public abstract LifeState LifeState { get; }
+
+    protected FrameObject(HostScreen screen)
+    {
+        ArgumentNullException.ThrowIfNull(screen);
+        _screen = screen;
+    }
+}
+
+public abstract class FrameObject<TLayer, TVertex, TShader, TMaterial> : FrameObject
+    where TLayer : ObjectLayer<TLayer, TVertex, TShader, TMaterial>
+    where TVertex : unmanaged, IVertex<TVertex>
+    where TShader : Shader<TShader, TMaterial>
+    where TMaterial : Material<TMaterial, TShader>
+{
+    private readonly TLayer _layer;
+    private LifeState _state;
+    private bool _isFrozen;
+
+    public TLayer Layer => _layer;
+    public sealed override LifeState LifeState => _state;
+
+    public sealed override bool IsFrozen
+    {
+        get => _isFrozen;
+        set => _isFrozen = value;
+    }
+
+    protected FrameObject(TLayer layer) : base(layer.Screen)
+    {
+        _layer = layer;
+        _isFrozen = false;
+        _state = LifeState.New;
+        layer.Add(this);
+    }
+
+    internal void SetLifeStateAlive()
+    {
+        Debug.Assert(_state == LifeState.New);
+        _state = LifeState.Alive;
+    }
+
+    internal void SetLifeStateDead()
+    {
+        Debug.Assert(_state == LifeState.Terminating);
+        _state = LifeState.Dead;
+    }
+
+    internal virtual void OnDead()
+    {
+        Debug.Assert(_state == LifeState.Dead);
+    }
+}
---
+++ WgpuSample/Elffy.Core/Positionable.cs
@@ -0,0 +1,37 @@
+﻿#nullable enable
+
+namespace Elffy;
+
+public abstract class Positionable<TLayer, TVertex, TShader, TMaterial>
+    : FrameObject<TLayer, TVertex, TShader, TMaterial>
+    where TLayer : ObjectLayer<TLayer, TVertex, TShader, TMaterial>
+    where TVertex : unmanaged, IVertex<TVertex>
+    where TShader : Shader<TShader, TMaterial>
+    where TMaterial : Material<TMaterial, TShader>
+{
+    private Vector3 _position;
+    private Quaternion _rotation;
+    private Vector3 _scale;
+
+    public Vector3 Position
+    {
+        get => _position;
+        set => _position = value;
+    }
+
+    public Quaternion Rotation
+    {
+        get => _rotation;
+        set => _rotation = value;
+    }
+
+    public Vector3 Scale
+    {
+        get => _scale;
+        set => _scale = value;
+    }
+
+    protected Positionable(TLayer layer) : base(layer)
+    {
+    }
+}
