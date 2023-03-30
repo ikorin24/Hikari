6df3ae40c4d645fa6fd868cff915fd342da29414
ikorin24 <sunshinexxab@yahoo.co.jp>
2023-03-30 23:24:13 +0900

    update

--- WgpuSample/Elffy.Core/IVertex.cs
+++ WgpuSample/Elffy.Core/IVertex.cs
@@ -11,3 +11,23 @@ public interface IVertex<TSelf>
 }
 
 public record struct VertexField(u32 Offset, u32 Size, VertexFormat Format, VertexFieldSemantics Semantics);
+
+public enum VertexFieldSemantics
+{
+    /// <summary>The field should be <see cref="Vector3"/> or same. (Format = <see cref="VertexFormat.Float32x3"/>)</summary>
+    Position = 0,
+    /// <summary>The field should be <see cref="Vector2"/>. (Format = <see cref="VertexFormat.Float32x2"/>)</summary>
+    UV,
+    /// <summary>The field should be <see cref="Vector3"/>. (Format = <see cref="VertexFormat.Float32x3"/>)</summary>
+    Normal,
+    /// <summary>The field should be <see cref="Vector4"/>. (Format = <see cref="VertexFormat.Float32x4"/>)</summary>
+    Color,
+    /// <summary>The field should be <see cref="uint"/>. (Format = <see cref="VertexFormat.Uint32"/>)</summary>
+    TextureIndex,
+    /// <summary>The field should be <see cref="Vector4u"/>. (Format = <see cref="VertexFormat.Uint32x4"/>)</summary>
+    Bone,
+    /// <summary>The field should be <see cref="Vector4"/>. (Format = <see cref="VertexFormat.Float32x4"/>)</summary>
+    Weight,
+    /// <summary>The field should be <see cref="Vector3"/>. (Format = <see cref="VertexFormat.Float32x3"/>)</summary>
+    Tangent,
+}
