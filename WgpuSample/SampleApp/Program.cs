728d960ffc013fd3dad13cf7851b02d4fc9c8e54
ikorin24 <sunshinexxab@yahoo.co.jp>
2023-04-17 18:55:07 +0900

    update

--- WgpuSample/SampleApp/Program.cs
+++ WgpuSample/SampleApp/Program.cs
@@ -42,18 +42,12 @@ internal class Program
         var albedo = LoadTexture(screen, "resources/ground_0036_color_1k.jpg", true);
         var mr = LoadTexture(screen, "pic.png", false);
         var normal = LoadTexture(screen, "resources/ground_0036_normal_opengl_1k.png", false);
-
-        //using var a = LoadTexture(screen, "resources/ground_0036_roughness_1k.jpg", TextureFormat.Rgba8UnormSrgb, true, (image, arg) =>
-        //{
-        //    var (mipLevel, texture) = arg;
-        //    texture.Write(mipLevel, image.GetPixels());
-        //});
-
-        var mesh = SampleData.SampleMesh(screen);
+        //var mesh = SampleData.SampleMesh(screen);
+        var mesh = Shapes.Cube(screen);
 
         var model = new PbrModel(layer, mesh, sampler, albedo, mr, normal);
         var camera = screen.Camera;
-        camera.Position = new Vector3(0, 0, 3);
+        camera.Position = new Vector3(2, 6, 9);
         camera.LookAt(Vector3.Zero);
     }
 
@@ -69,69 +63,100 @@ internal class Program
             return Image.FromStream(stream, Path.GetExtension(filepath));
         }
     }
+}
 
-    //private static Own<Texture> LoadTexture(Screen screen, string filepath, bool isSrgb, bool useMipmap)
-    //{
-    //    using var image = LoadImage(filepath);
-    //    var format = isSrgb ? TextureFormat.Rgba8UnormSrgb : TextureFormat.Rgba8Unorm;
-    //    var mipLevelCount = useMipmap ? uint.Log2(uint.Min((uint)image.Width, (uint)image.Height)) : 1;
-    //    var texture = Texture.Create(screen, new TextureDescriptor
-    //    {
-    //        Size = new Vector3u((uint)image.Width, (uint)image.Height, 1),
-    //        MipLevelCount = mipLevelCount,
-    //        SampleCount = 1,
-    //        Dimension = TextureDimension.D2,
-    //        Format = format,
-    //        Usage = TextureUsages.TextureBinding | TextureUsages.CopyDst,
-    //    });
-    //    var tex = texture.AsValue();
-    //    tex.Write<ColorByte>(0, image.GetPixels());
-    //    for(uint i = 1; i < tex.MipLevelCount; i++) {
-    //        var w = (image.Width >> (int)i);
-    //        var h = (image.Height >> (int)i);
-    //        using var curuent = image.Resized(new Vector2i(w, h));
-    //        tex.Write<ColorByte>(i, curuent.GetPixels());
-    //    }
-    //    return texture;
+public static class Shapes
+{
+    public static Own<Mesh<Vertex>> Cube(Screen screen)
+    {
+        // [indices]
+        //             0 ------- 3
+        //             |         |
+        //             |   up    |
+        //             |         |
+        //             1 ------- 2
+        // 4 ------- 7 8 -------11 12-------15 16-------19
+        // |         | |         | |         | |         |
+        // |  left   | |  front  | |  right  | |  back   |
+        // |         | |         | |         | |         |
+        // 5 ------- 6 9 -------10 13-------14 17-------18
+        //             20-------23
+        //             |         |
+        //             |  down   |
+        //             |         |
+        //             21-------22
+        //
+        // [uv]
+        // OpenGL coordinate of uv is left-bottom based,
+        // but many popular format of images (e.g. png) are left-top based.
+        // So, I use left-top as uv coordinate.
+        //
+        //       0 ------ 1/4 ----- 1/2 ----- 3/4 ------ 1
+        //
+        //   0   o --> u   + ------- +
+        //   |   |         |         |
+        //   |   v         |   up    |
+        //   |             |         |
+        //  1/3  + ------- + ------- + ------- + ------- +
+        //   |   |         |         |         |         |
+        //   |   |  left   |  front  |  right  |  back   |
+        //   |   |         |         |         |         |
+        //  2/3  + ------- + ------- + ------- + ------- +
+        //   |             |         |
+        //   |             |  down   |
+        //   |             |         |
+        //   1             + ------- +
+        //
+        //     + ------- +
+        //    /   up    /|
+        //   + ------- + |
+        //   |         | ← right
+        //   |  front  | +
+        //   |         |/
+        //   + ------- +
 
-    //    static Image LoadImage(string filepath)
-    //    {
-    //        using var stream = File.OpenRead(filepath);
-    //        return Image.FromStream(stream, Path.GetExtension(filepath));
-    //    }
-    //}
+        const float a = 0.5f;
+        const float b0 = 0f;
+        const float b1 = 1f / 4f;
+        const float b2 = 2f / 4f;
+        const float b3 = 3f / 4f;
+        const float b4 = 1f;
+        const float c0 = 0f;
+        const float c1 = 1f / 3f;
+        const float c2 = 2f / 3f;
+        const float c3 = 1f;
 
-    //private static Own<Texture> LoadTexture(Screen screen, string filepath, TextureFormat format, bool useMipmap, ReadOnlyImageAction<(uint MipLevel, Texture Texture)> initTexture)
-    //{
-    //    using var image = LoadImage(filepath);
-    //    var mipLevelCount = useMipmap ? uint.Log2(uint.Min((uint)image.Width, (uint)image.Height)) : 1;
-    //    var texture = Texture.Create(screen, new TextureDescriptor
-    //    {
-    //        Size = new Vector3u((uint)image.Width, (uint)image.Height, 1),
-    //        MipLevelCount = mipLevelCount,
-    //        SampleCount = 1,
-    //        Dimension = TextureDimension.D2,
-    //        Format = format,
-    //        Usage = TextureUsages.TextureBinding | TextureUsages.CopyDst,
-    //    });
-    //    var tex = texture.AsValue();
-    //    initTexture.Invoke(image, (MipLevel: 0, Texture: tex));
+        ReadOnlySpan<Vertex> vertices = stackalloc Vertex[24]
+        {
+            new Vertex(new(-a, a, -a), new(0, 1, 0), new(b1, c0)),
+            new Vertex(new(-a, a, a), new(0, 1, 0), new(b1, c1)),
+            new Vertex(new(a, a, a), new(0, 1, 0), new(b2, c1)),
+            new Vertex(new(a, a, -a), new(0, 1, 0), new(b2, c0)),
+            new Vertex(new(-a, a, -a), new(-1, 0, 0), new(b0, c1)),
+            new Vertex(new(-a, -a, -a), new(-1, 0, 0), new(b0, c2)),
+            new Vertex(new(-a, -a, a), new(-1, 0, 0), new(b1, c2)),
+            new Vertex(new(-a, a, a), new(-1, 0, 0), new(b1, c1)),
+            new Vertex(new(-a, a, a), new(0, 0, 1), new(b1, c1)),
+            new Vertex(new(-a, -a, a), new(0, 0, 1), new(b1, c2)),
+            new Vertex(new(a, -a, a), new(0, 0, 1), new(b2, c2)),
+            new Vertex(new(a, a, a), new(0, 0, 1), new(b2, c1)),
+            new Vertex(new(a, a, a), new(1, 0, 0), new(b2, c1)),
+            new Vertex(new(a, -a, a), new(1, 0, 0), new(b2, c2)),
+            new Vertex(new(a, -a, -a), new(1, 0, 0), new(b3, c2)),
+            new Vertex(new(a, a, -a), new(1, 0, 0), new(b3, c1)),
+            new Vertex(new(a, a, -a), new(0, 0, -1), new(b3, c1)),
+            new Vertex(new(a, -a, -a), new(0, 0, -1), new(b3, c2)),
+            new Vertex(new(-a, -a, -a), new(0, 0, -1), new(b4, c2)),
+            new Vertex(new(-a, a, -a), new(0, 0, -1), new(b4, c1)),
+            new Vertex(new(-a, -a, a), new(0, -1, 0), new(b1, c2)),
+            new Vertex(new(-a, -a, -a), new(0, -1, 0), new(b1, c3)),
+            new Vertex(new(a, -a, -a), new(0, -1, 0), new(b2, c3)),
+            new Vertex(new(a, -a, a), new(0, -1, 0), new(b2, c2)),
+        };
 
-    //    //tex.Write<ColorByte>(0, image.GetPixels());
+        // up, left, front, right, back, down
+        ReadOnlySpan<ushort> indices = stackalloc ushort[36] { 0, 1, 2, 2, 3, 0, 4, 5, 6, 6, 7, 4, 8, 9, 10, 10, 11, 8, 12, 13, 14, 14, 15, 12, 16, 17, 18, 18, 19, 16, 20, 21, 22, 22, 23, 20 };
 
-    //    for(uint i = 1; i < tex.MipLevelCount; i++) {
-    //        var w = (image.Width >> (int)i);
-    //        var h = (image.Height >> (int)i);
-    //        using var curuent = image.Resized(new Vector2i(w, h));
-    //        initTexture.Invoke(curuent, (MipLevel: i, Texture: tex));
-    //        //tex.Write<ColorByte>(i, curuent.GetPixels());
-    //    }
-    //    return texture;
-
-    //    static Image LoadImage(string filepath)
-    //    {
-    //        using var stream = File.OpenRead(filepath);
-    //        return Image.FromStream(stream, Path.GetExtension(filepath));
-    //    }
-    //}
+        return Mesh.CreateWithTangent(screen, vertices, indices);
+    }
 }
